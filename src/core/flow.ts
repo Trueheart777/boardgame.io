Trueheart777/boardgame.io

commit b9b6952459311ddeccb7a8ef4f5c20a71009b898

feat(core): Expose `ctx.playerID` in `onMove` hook (#1019)

src/core/flow.ts
@@ -727,7 +727,10 @@ export function Flow({
     }
 
     const phaseConfig = GetPhase(ctx);
-    const G = phaseConfig.turn.wrapped.onMove(state);
+    const G = phaseConfig.turn.wrapped.onMove({
+      ...state,
+      ctx: { ...ctx, playerID },
+    });
     state = { ...state, G };
 
     const events = [{ fn: OnMove }];

src/core/flow.test.ts
@@ -217,17 +217,17 @@ describe('turn', () => {
     expect(onEnd).toHaveBeenCalled();
   });
 
-  test('onMove', () => {
+  describe('onMove', () => {
     const onMove = () => ({ A: true });
 
-    {
+    test('top level callback', () => {
       const flow = Flow({ turn: { onMove } });
       let state = { G: {}, ctx: flow.ctx(2) } as State;
       state = flow.processMove(state, makeMove('').payload);
       expect(state.G).toEqual({ A: true });
-    }
+    });
 
-    {
+    test('phase specific callback', () => {
       const flow = Flow({
         turn: { onMove },
         phases: { B: { turn: { onMove: () => ({ B: true }) } } },
@@ -238,7 +238,20 @@ describe('turn', () => {
       state = flow.processEvent(state, gameEvent('setPhase', 'B'));
       state = flow.processMove(state, makeMove('').payload);
       expect(state.G).toEqual({ B: true });
-    }
+    });
+
+    test('ctx with playerID', () => {
+      const playerID = 'playerID';
+      const flow = Flow({
+        turn: { onMove: (G, ctx) => ({ playerID: ctx.playerID }) },
+      });
+      let state = { G: {}, ctx: flow.ctx(2) } as State;
+      state = flow.processMove(
+        state,
+        makeMove('', undefined, 'playerID').payload
+      );
+      expect(state.G.playerID).toEqual(playerID);
+    });
   });
 
   describe('minMoves', () => {

commit 17855101a84a30a00941bbe9ba11a389b8371cef

fix(flow): Pass correct `ctx` to `onMove` hooks
Addresses a regression introduced in #1019, which passed a stale version of `ctx` when calling `onMove` hooks. This was particularly noticeable if the move triggered the end of a player’s stage, which the `onMove` hook would then not see.

src/core/flow.ts
@@ -698,13 +698,12 @@ export function Flow({
 
   function ProcessMove(state: State, action: ActionPayload.MakeMove): State {
     const { playerID, type } = action;
-    const { ctx } = state;
-    const { currentPlayer, activePlayers, _activePlayersMaxMoves } = ctx;
-    const move = GetMove(ctx, type, playerID);
+    const { currentPlayer, activePlayers, _activePlayersMaxMoves } = state.ctx;
+    const move = GetMove(state.ctx, type, playerID);
     const shouldCount =
       !move || typeof move === 'function' || move.noLimit !== true;
 
-    let { numMoves, _activePlayersNumMoves } = ctx;
+    let { numMoves, _activePlayersNumMoves } = state.ctx;
     if (shouldCount) {
       if (playerID === currentPlayer) numMoves++;
       if (activePlayers) _activePlayersNumMoves[playerID]++;
@@ -713,7 +712,7 @@ export function Flow({
     state = {
       ...state,
       ctx: {
-        ...ctx,
+        ...state.ctx,
         numMoves,
         _activePlayersNumMoves,
       },
@@ -726,10 +725,10 @@ export function Flow({
       state = EndStage(state, { playerID, automatic: true });
     }
 
-    const phaseConfig = GetPhase(ctx);
+    const phaseConfig = GetPhase(state.ctx);
     const G = phaseConfig.turn.wrapped.onMove({
       ...state,
-      ctx: { ...ctx, playerID },
+      ctx: { ...state.ctx, playerID },
     });
     state = { ...state, G };
 